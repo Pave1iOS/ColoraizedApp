2fb4c8e34bf54193d10669b3d37bdc561b6afb1a
Pave1iOS <PGribachev94@yandex.ru>
2024-03-04 16:23:33 +0300

    Initial Commit

---
+++ ColoraizedApp/ColoraizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoraizedAppApp.swift
+//  ColoraizedApp
+//
+//  Created by Pavel Gribachev on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColoraizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColoraizedApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColoraizedApp
+//
+//  Created by Pavel Gribachev on 04.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
