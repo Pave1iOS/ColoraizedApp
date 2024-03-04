0128bfe11670cea5ab9225a26d3f51cc04982161
Pave1iOS <PGribachev94@yandex.ru>
2024-03-04 18:25:52 +0300

    Step 1

--- ColoraizedApp/ContentView.swift
+++ ColoraizedApp/ContentView.swift
@@ -8,16 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            VStack {
+                RectangleView(color: .blue)
+                    .padding(.bottom, 40)
+                
+                VStack(spacing: 10) {
+                    
+                    SliderView(value: $redSliderValue, color: .red)
+                    SliderView(value: $greenSliderValue, color: .green)
+                    SliderView(value: $blueSliderValue, color: .blue)
+                    
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
+
+struct SliderView: View {
+    @Binding var value: Double
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            Text( lround(value).formatted())
+                .frame(width: 35, height: 40)
+                .foregroundStyle(.white)
+                .font(.headline)
+                .fontWeight(.heavy)
+                .padding([.trailing, .leading], 5)
+            
+            Slider(value: $value, in: 0...255, step: 1)
+                .tint(color)
+                .padding(.trailing, 20)
+        }
+            
+    }
+}
+
+struct RectangleView: View {
+    let color: Color
+    
+    var body: some View {
+        Rectangle()
+            .frame(width: 250, height: 120)
+            .foregroundStyle(color)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 4)
+                    .fill(.white))
+    }
+}
+
 
 #Preview {
     ContentView()
