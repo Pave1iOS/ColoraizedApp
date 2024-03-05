2c43bed32b1fe22bd70407a27dc94c4fee45aad4
Pave1iOS <PGribachev94@yandex.ru>
2024-03-06 00:56:34 +0300

    code review

--- ColoraizedApp/SliderView.swift
+++ ColoraizedApp/SliderView.swift
@@ -14,47 +14,69 @@ struct SliderView: View {
         }
     }
     @State private var afterValue: Double = 0
-    
-    @State private var editing = false
-    @State private var isPresented = false
-    
+
     let color: Color
         
     var body: some View {
         HStack {
-            Text(lround(sliderValue).formatted())
-                .frame(width: 35, height: 40)
-                .foregroundStyle(.white)
-                .font(.headline)
-                .fontWeight(.heavy)
-                .padding([.trailing, .leading], 5)
+            TextView(sliderValue: sliderValue)
             
             Slider(value: $sliderValue, in: 0...255, step: 1)
                 .tint(color)
                 .padding(.trailing, 10)
             
-            TextField("", value: editing ? $afterValue : $sliderValue, formatter: NumberFormatter(), onEditingChanged: { isEditing in
-                sliderValue = afterValue
-                editing = isEditing
-                isValidation()
-            }).alert("Wrong Format!", isPresented: $isPresented) {                Button("OK") {
-                    sliderValue = 100
-                    afterValue = 100
-                }
-            } message: {
-                Text("please enter a valid value from 0 to 255")
-            }
-                .frame(width: 50, height: 40)
-                .keyboardType(.numberPad)
-                .foregroundStyle(.black)
-                .textFieldStyle(.roundedBorder)
-                .multilineTextAlignment(.trailing)
-                .padding(.trailing, 15)
+            TextFieldView(sliderValue: $sliderValue)
         }
     }
 }
 
-private extension SliderView {
+struct TextFieldView: View {
+    @Binding var sliderValue: Double {
+        willSet {
+            afterValue = sliderValue
+        }
+    }
+    
+    @State private var afterValue: Double = 0
+    
+    @State private var isPresented = false
+    @State private var editing = false
+    
+    var body: some View {
+        TextField("", value: editing ? $afterValue : $sliderValue, formatter: NumberFormatter(), onEditingChanged: { isEditing in
+            sliderValue = afterValue
+            editing = isEditing
+            isValidation()
+        }).alert("Wrong Format!", isPresented: $isPresented) {                Button("OK") {
+                sliderValue = 100
+                afterValue = 100
+            }
+        } message: {
+            Text("please enter a valid value from 0 to 255")
+        }
+            .frame(width: 50, height: 40)
+            .keyboardType(.numberPad)
+            .foregroundStyle(.black)
+            .textFieldStyle(.roundedBorder)
+            .multilineTextAlignment(.trailing)
+            .padding(.trailing, 15)
+    }
+}
+
+struct TextView: View {
+    let sliderValue: Double
+    
+    var body: some View {
+        Text(lround(sliderValue).formatted())
+            .frame(width: 35, height: 40)
+            .foregroundStyle(.white)
+            .font(.headline)
+            .fontWeight(.heavy)
+            .padding([.trailing, .leading], 5)
+    }
+}
+
+private extension TextFieldView {
     func isValidation() {
         if !(0...255).contains(sliderValue) {
             isPresented = true
